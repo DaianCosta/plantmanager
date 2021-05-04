e2fed268a2b3d8389dc298f06d2c1ce72e3a4513
Daian Costa <daiancosta@Daians-MacBook-Pro.local>
2021-05-03 23:21:00 -0300

    add

---
+++ App.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+import AppLoading from "expo-app-loading";
+import * as Notifications from "expo-notifications";
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold,
+} from "@expo-google-fonts/jost";
+import Routes from "./src/routes";
+import { PlantProps } from "./src/libs/storage";
+
+export default function App() {
+  useEffect(() => {
+    const subscription = Notifications.addNotificationReceivedListener(
+      async (notification) => {
+        const data = notification.request.content.data.plant as PlantProps;
+        console.log(data);
+      }
+    );
+    return () => subscription.remove();
+    // async function notifications() {
+    //   const data = await Notifications.getAllScheduledNotificationsAsync();
+    //   console.log("#### notifications ####");
+    //   console.log(data);
+    // }
+  }, []);
+
+  const [fontsLoaded] = useFonts({ Jost_400Regular, Jost_600SemiBold });
+
+  if (!fontsLoaded) return <AppLoading />;
+
+  return <Routes />;
+}
